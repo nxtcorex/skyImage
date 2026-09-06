1b98b19d5aa35ed5b5ad90aaec9d1d306b0458cc
fishcpy <fishcpy@foxmail.com>
2026-09-06 13:18:00 +0800

    feat:更新0.2.4

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version defines the current release identifier returned to the UI.
-const Version = "0.2.3"
+const Version = "0.2.4"
 
 // About defines the project description shown on the about page.
 const About = "skyImage-云端图床"
